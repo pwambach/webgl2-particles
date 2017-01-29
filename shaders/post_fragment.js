e3deadf2f1eb2c401c7f486d0e2db411de4ce074
Philipp Wambach <philippwambach@googlemail.com>
2017-01-29 14:16:58 +0100

    change postpro colors

--- shaders/post_fragment.js
+++ shaders/post_fragment.js
@@ -5,18 +5,19 @@ const fragmentPostShader = `#version 300 es
   out vec4 color;
 
   void main() {
-    float texColor = texture(tex, v_uv / 2.0 + 0.5).r;
-    
-    vec3 outColor = vec3(texColor);
-    
-    if (texColor < 0.08) {
-      outColor = vec3(1.0 - texColor, 0.0, 0.0);
+    vec4 texColor = texture(tex, v_uv / 2.0 + 0.5);
+    float intensity = 1.0 - texColor.r;
+
+    vec3 finalColor = texColor.rgb;
+
+    if (intensity > 0.5) {
+      finalColor.r *= 3.0;
+      finalColor.b *= finalColor.r;
+      finalColor.g = 1.0 - finalColor.b;
+      finalColor.r = 1.0 - finalColor.r * finalColor.g;
+      finalColor.rgb = finalColor.brg;
     }
 
-    if (texColor < 0.02) {
-      outColor = vec3(1.0, 1.0, 0.0);
-    }
-
-    color = vec4(outColor, 1.0);
+    color = vec4(finalColor, 1.0);
   }
 `;
